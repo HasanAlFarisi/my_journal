d4a7375c6fe418267856f0cc170a5be0d41d3a51
Hasan Al Farisi <hasan.farisi47@gmail.com>
2014-12-30 14:17:50 +0700

    Fix bug in Journal

--- app/controllers/admin/journals_controller.rb
+++ app/controllers/admin/journals_controller.rb
@@ -7,10 +7,6 @@ class Admin::JournalsController < Admin::BaseController
   # GET /admin/journals
   # GET /admin/journals.json
   def index
-    Admin::Journal.delete_all
-    Admin::JournalTeamDeveloper.delete_all
-    Admin::JournalTeamCheck.delete_all
-    Admin::JornalTeamDesigner.delete_all
     @admin_journals = Admin::Journal.order("admin_journals.created_at DESC").paginate(:page => params[:page], :per_page => 6).is_allowed(current_admin.id,nil)
     ids = []
     @admin_journals.each do |journal|
